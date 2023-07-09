8f27964d98f8b9ef9b69c4e6bb41d4c45484f570
Waseem A. Abd Elhadi <112069422+Waseemko-ayman@users.noreply.github.com>
2023-07-09 13:38:53 +0300

    edit validation form

--- src/Pages/LoginPage/index.jsx
+++ src/Pages/LoginPage/index.jsx
@@ -7,12 +7,82 @@ import Button from '../../Components/Button';
 
 class LoginPage extends Component {
   state = {
-
+    username: '',
+    email: '',
+    phone: '',
+    password: '',
+    repeatPassword: '',
+    myData: "initialData",
+    agreeTerms: false,
+    errors: {},
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
+    const errors = this.validateForm();
+    if (Object.keys(errors).length !== 0) {
+      this.setState({ errors });
+    }
   }
+
+  validateForm = () => {
+    const { username, email, phone, password, repeatPassword, agreeTerms } = this.state;
+    const errors = {};
+  
+    // Validate username
+    if (username.trim() === '') {
+      errors.username = 'Username is required';
+    }
+  
+    // Validate email
+    if (email.trim() === '') {
+      errors.email = 'Email is required';
+    } else if (!this.isValidEmail(email)) {
+      errors.email = 'Invalid email format';
+    }
+  
+    // Validate phone
+    if (phone.trim() === '') {
+      errors.phone = 'Phone is required';
+    }
+  
+    // Validate password
+    if (password.trim() === '') {
+      errors.password = 'Password is required';
+    } else if (password.length > 6) {
+      errors.password = 'Password must be at least 6 characters long';
+    }
+  
+    // Validate repeat password
+    if (repeatPassword.trim() === '') {
+      errors.repeatPassword = 'Repeat password is required';
+    } else if (password !== repeatPassword) {
+      errors.repeatPassword = 'Passwords do not match';
+    }
+  
+    // Validate agree terms
+    if (!agreeTerms) {
+      errors.agreeTerms = 'You must agree to the terms and conditions';
+    }
+    return errors;
+  };
+
+  isValidEmail = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleCheckboxChange = (e) => {
+    this.setState((prevState) => ({
+      agreeTerms: !prevState.agreeTerms,
+    }));
+  };
 
   render() {
     return (
@@ -33,14 +103,63 @@ class LoginPage extends Component {
                 <p>For the purpose of gamers regulation, your details are required.</p>
               </div>
               <form action="" onSubmit={this.handleSubmit}>
-                <Inputs label="Username*" type="text" placeholder="Enter username" imageHidden />
-                <Inputs label="Email address*" type="email" placeholder="Enter email address" imageHidden />
-                <Inputs label="Phone*" type="text" placeholder="Enter phone" imageHidden />
-                <Inputs label="Create Password*" type="password" placeholder="Password" imageSrc="/assets/eye.svg" />
-                <Inputs label="Repeat password*" type="password" placeholder="Repeat password" imageSrc="/assets/eye.svg" />
+                <Inputs 
+                  value={this.state.username} 
+                  onChange={this.handleInputChange} 
+                  label="Username*" 
+                  type="text" 
+                  placeholder="Enter username" 
+                  imageHidden 
+                />
+                {this.state.errors.username && <p className="error">{this.state.errors.username}</p>}
+                <Inputs 
+                  value={this.state.email} 
+                  onChange={this.handleInputChange} 
+                  label="Email address*" 
+                  type="email" 
+                  placeholder="Enter email address" 
+                  imageHidden 
+                />
+                {this.state.errors.email && <p className="error">{this.state.errors.email}</p>}
+                <Inputs 
+                  value={this.state.phone} 
+                  onChange={this.handleInputChange} 
+                  label="Phone*" 
+                  type="text" 
+                  placeholder="Enter phone" 
+                  imageHidden 
+                />
+                {this.state.errors.phone && <p className="error">{this.state.errors.phone}</p>}
+                <Inputs 
+                  value={this.state.password} 
+                  onChange={this.handleInputChange} 
+                  label="Create Password*" 
+                  type="password" 
+                  placeholder="Password" 
+                  imageSrc="/assets/eye.svg" 
+                />
+                {this.state.errors.password && <p className="error">{this.state.errors.password}</p>}
+                <Inputs 
+                  value={this.state.repeatPassword} 
+                  onChange={this.handleInputChange} 
+                  label="Repeat password*" 
+                  type="password" 
+                  placeholder="Repeat password" 
+                  imageSrc="/assets/eye.svg" 
+                />
+                {this.state.errors.repeatPassword && <p className="error">{this.state.errors.repeatPassword}</p>}
                 <div className='checkbox'>
-                  <input type="checkbox" id="checkbox" defaultChecked />
-                  <label htmlFor="checkbox">I agree to terms & conditions</label>
+                  <div>
+                    <input 
+                      type="checkbox" 
+                      id="checkbox" 
+                      checked={this.state.agreeTerms} 
+                      onChange={this.handleCheckboxChange} 
+                      defaultChecked 
+                    />
+                    <label htmlFor="checkbox">I agree to terms & conditions</label>
+                  </div>
+                  {this.state.errors.repeatPassword && <p className="error">{this.state.errors.agreeTerms}</p>}
                 </div>
                 <div className="register">
                   <Button btnText="Register Account" />
